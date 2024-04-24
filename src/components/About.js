1f3d737933c00e69cb41b9e7924c8a55d749fae0
meshackmesto <meshackorina93@gmail.com>
2024-04-24 10:46:43 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">About
+  <h2>About Me</h2>
+  <p>Nothing about jus jus gimme my money!!</p>
+  <img src={image} alt="I made this"/>
+  </div>;
 }
 
 export default About;
